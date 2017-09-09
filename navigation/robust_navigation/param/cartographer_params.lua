28f48bc64aa28d51493aabfa20d5607b95a08557
Anvith Ekkati <eanvith@gmail.com>
2017-09-09 17:58:21 -0400

    params updated

--- navigation/robust_navigation/param/cartographer_params.lua
+++ navigation/robust_navigation/param/cartographer_params.lua
@@ -28,7 +28,6 @@ options = {
   num_multi_echo_laser_scans = 0,
   num_subdivisions_per_laser_scan = 1,
   num_point_clouds = 0,
-  num_point_clouds = 0,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
@@ -36,36 +35,42 @@ options = {
 }
 
 MAP_BUILDER.use_trajectory_builder_2d = true
-
--- These had their names changed by the latest version of cartographer
 TRAJECTORY_BUILDER_2D.min_range = 0.2
 TRAJECTORY_BUILDER_2D.max_range = 4.
 TRAJECTORY_BUILDER_2D.missing_data_ray_length = 3.
-
--- Your IMU was not helping. I disabled it and instead switched to the correlative scan matcher (i.e. only use laser information).
 TRAJECTORY_BUILDER_2D.use_imu_data = false
 TRAJECTORY_BUILDER_2D.use_online_correlative_scan_matching = true
+TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
+SPARSE_POSE_GRAPH.constraint_builder.min_score = 0.8
+SPARSE_POSE_GRAPH.constraint_builder.global_localization_min_score = 0.8
+
+SPARSE_POSE_GRAPH.optimize_every_n_scans = 0
+
+--[[
+TRAJECTORY_BUILDER_2D.submaps.resolution = 0.035
+TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.1
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 70
+TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 300
+SPARSE_POSE_GRAPH.constraint_builder.ceres_scan_matcher = TRAJECTORY_BUILDER_2D.ceres_scan_matcher
+SPARSE_POSE_GRAPH.optimization_problem.huber_scale = 1e2
+]]--
+-- These had their names changed by the latest version of cartographer
+
+
+-- Your IMU was not helping. I disabled it and instead switched to the correlative scan matcher (i.e. only use laser information).
 
 -- I made the submaps slightly smaller and since the laser is so good I
 -- increased the resoultion to see more fine grained features.
-TRAJECTORY_BUILDER_2D.submaps.resolution = 0.035
 -- This seems to be depricated in the latest version of cartographer
 --TRAJECTORY_BUILDER_2D.submaps.num_laser_fans = 60
-SPARSE_POSE_GRAPH.optimize_every_n_scans = 60
 
-TRAJECTORY_BUILDER_2D.motion_filter.max_angle_radians = math.rad(0.1)
-TRAJECTORY_BUILDER_2D.motion_filter.max_distance_meters = 0.1
+
+
 
 -- Trust the constant velocity model more (this is putting more weight on odometry)
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.translation_weight = 70
-TRAJECTORY_BUILDER_2D.ceres_scan_matcher.rotation_weight = 300
 
-SPARSE_POSE_GRAPH.constraint_builder.min_score = 0.8
 
 -- Use the same scan matcher settings for loop closure as for the local SLAM
-SPARSE_POSE_GRAPH.constraint_builder.ceres_scan_matcher = TRAJECTORY_BUILDER_2D.ceres_scan_matcher
-SPARSE_POSE_GRAPH.constraint_builder.global_localization_min_score = 0.8
-SPARSE_POSE_GRAPH.optimization_problem.huber_scale = 1e2
 
 return options
 
